8ec23624c6c7a14f876c9d4cabcae40517c1879f
will-k-minor <willkminor@gmail.com>
2024-09-03 19:41:11 -0500

    Initial commit for NextJS project

---
+++ src/app/search/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+const Search: React.FC = () => {
+    return (
+        <div>
+            <h1>Search Page</h1>
+            {/* Add your search functionality here */}
+        </div>
+    );
+};
+
+export default Search;
